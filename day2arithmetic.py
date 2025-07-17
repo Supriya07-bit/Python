f20b58294ac5caa0e5448397a6f2d73a1afa29d3
Supriya07-bit <supriyakollure@gmail.com>
2025-07-17 13:38:52 +0530

    Add files via upload

---
+++ day2arithmetic.py
@@ -0,0 +1,8 @@
+a = 4
+b = 2
+print("Addition:" , a + b)
+print("Subtraction:" , a - b)
+print("Multiplication:" , a * b)
+print("Division:" , a / b)
+print("Modulus:" , a % b)
+print("Power:" , a ** b)
